4137178219692e880d1411806d58aa1bfa8afe50
BLacuesta3 <lvnlacuesta@gmail.com>
2024-04-24 21:05:07 -0700

    project commit

--- README.md
+++ README.md
@@ -1,15 +1,30 @@
-# sprint_4_project
+Project Description:  This application is named 'Car Data Analysis'.  This application contains: a display of the
+full vehicles_us dataframe, a scatterplot titled: 'Car Price/Quality Analysis' which displays the relationship between the car price/quality and the number of days listed in order to show whether car price or car quality affects sales, a histogram titled: 'Car Model Count' which displays the amount of cars listed in the company according to car model and price, and a checkbox which displays the company phone number(fake number) if the box is checked.
 
-# Working with Python environment
-1. Create an environment. In Git Bash terminal, run:
-```bash
-python -m venv venv  # last is the name of environment folder
-```
-2. To activate the environment, run:
-```bash
-source venv/Scripts/activate
-```
-3. Instasll libraries specified int the requirements.txt file
-```bash
-pip install -r requirements.txts
-```
+Libraries imported:  Pandas in order to import the dataframe.  Streamlit in order to run the application.  Plotly Express in order to display the chart.
+
+List of functions used:
+Python:
+* fillna() - in order to fill in missing values in the dataframe
+* unique() - in order to display all unique values
+Pandas:
+* pd.read_csv() - in order to import the dataframe
+Plotly Express:
+* px.scatter() - in order to create the scatterplot
+* px.histogram() - in order to create the histogram
+Streamlit:
+* st.header() - in order to display a header with text
+* st.write() -
+* st.subheader() - in order to display a subheader with text
+* st.dataframe() - in order to display dataframe on the app
+* st.selectbox() - in order to display selectbox on the app
+* st.checkbox() - in order to display checkboxes on the app
+* st.plotly_chart() - in order to display charts from plotly express on the app
+
+Special Arguements:
+* animation_frame= and animation_group= to px.scatter() in order to make animated scatterplot
+
+Instructions on how to run the app
+Type in the following command in the terminal:
+
+streamlit run app.py
--- app.py
+++ app.py
@@ -6,25 +6,33 @@ import plotly.express as px
 df = pd.read_csv('vehicles_us.csv')
 df.fillna('unknown', inplace=True)
 
-#create a scatterplot that displays the how the the price of each car affects how long the car is listed for.
-fig_scatter_1 = px.scatter(df, x= 'price', y='days_listed',color='condition', title="Relationship Between Car Price/ Car Quality and Days Listed", labels={'price': 'Car Price', 'days_listed': 'Number of Days Listed'})
-#create a histogram that counts the amount of listings for each car model in the lot.
-fig_hist_1 = px.histogram(df,x='model', nbins=20, height=600, width=900, title='Number of Cars in Lot per Model')
+#create a scatterplot that displays the how the the price of each car affects how long the car is listed for.  Add animation frame that displays the type and model of each car.
+fig_scatter_1 = px.scatter(df, x= 'price', y='days_listed',color='condition', title="Relationship Between Car Price/ Car Quality and Days Listed", labels={'price': 'Car Price', 'days_listed': 'Number of Days Listed'},animation_frame='type', animation_group='model')
 
+st.header('Project Title: Car Data Analysis')
 #Display the dataframe on the application and create header.
 st.header('US Vehicles Dataframe')
 st.dataframe(df)
 
-#check = st.checkbox() *Code for the checkbox.
 #Display the previously mentioned scatterplot on the application and add a header.
 st.header('Car Price/Quality Analysis')
 st.plotly_chart(fig_scatter_1)
-#Display the previously mentioned histogram on the application and add a header.
-st.header('Car Model Count Histogram')
-st.plotly_chart(fig_hist_1)
 
+#Filter all the unique values using the unique() function on the model column of the df dataframe.
+model_unique = df['model'].unique()
+
+#Create Selectbox with the model_unique variable previously created.
+st.header("Car Model Count")
+st.subheader("This histogram allows you to select and view the amount of cars listed by model and price.")
+model_options = st.selectbox('Select Car Model', options=model_unique, index=0)
+
+#Create a histogram that filters all the differnt types of car model in the model column of the df dataframe.
+model_df = df[df['model']==model_options]
+model_hist = px.histogram(model_df, x='price', nbins=20, height=600, width=900, title='Price of Cars Per Model')
+st.write(model_hist)
+
+#Add a checkbox using st.checkbox that displays the company phone number if checked.
 st.header('Would you like to give our company a call?')
 check = st.checkbox("Yes, I would like the company phone number!")
 if check == True:
     st.write("Please call 1-800-4443 (This is not a real number)")
-st.write("Test")
